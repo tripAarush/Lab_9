d2ea883aaaa71962c379ee9706ae12d64bbb3436
nityealuru <109382466+nityealuru@users.noreply.github.com>
2024-04-13 14:28:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,17 @@ def encode(password):
     digit_encoded = [(digit + 3) % 10 for digit in digit_pw]
     pw_encoded = [str(digit) for digit in digit_encoded]
     return ''.join(pw_encoded)
+
+def decode(encoded_password):
+    decoded_password = ""
+    for char in encoded_password:
+        if char.isdigit():
+            decoded_digit = (int(char) - 3) % 10
+            decoded_password += str(decoded_digit)
+        else:
+            print("Error: Encoded password must contain only integers.")
+            return ""
+    return decoded_password
     
 def main():
     encoded = 0
