03f83f79303abcc371b1bfbc91ecbbf798f8d4bf
tripAarush <166939692+tripAarush@users.noreply.github.com>
2024-04-13 13:53:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 # first github
-print('hello')
+def encode(password):
+    if password.isdigit() or not len(password) != 8:
+        raise ValueError("Password should be 8 digits long and contain only integers.")
+
+    digit_pw = [int(digit) for digit in password]
+    digit_encoded = [(digit + 3) % 10 for digit in password_digits]
+    pw_encoded = [str(digit) for digit in encoded_digits]
+    return ''.join(pw_encoded)
