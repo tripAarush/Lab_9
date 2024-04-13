eb2f353ea0e8b561326b9ffbef05bd9cfe554b41
tripAarush <166939692+tripAarush@users.noreply.github.com>
2024-04-13 14:09:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 # first github
-# encode
 def encode(password):
-    if password.isdigit() or not len(password) != 8:
+    if not password.isdigit() or len(password) != 8:
         raise ValueError("Password should be 8 digits long and contain only integers.")
 
     digit_pw = [int(digit) for digit in password]
-    digit_encoded = [(digit + 3) % 10 for digit in password_digits]
-    pw_encoded = [str(digit) for digit in encoded_digits]
+    digit_encoded = [(digit + 3) % 10 for digit in digit_pw]
+    pw_encoded = [str(digit) for digit in digit_encoded]
     return ''.join(pw_encoded)
+    
+def main():
+    encoded = 0
+    while True:
+        print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit")
+        option = int(input("Please enter an option:"))
+        if option == 1:
+            pw = input("Please enter your password to encode:")
+            encoded = encode(pw)
+            print("Your password has been encoded and stored!")
+        elif option == 2:
+            print("The encoded password is", encoded, "and the original password is", pw)
+        elif option == 3:
+            break
+        else:
+            print("Invalid option.")
+
+if __name__ == "__main__":
+    main()
